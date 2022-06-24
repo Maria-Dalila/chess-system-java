80dafc7696b345390a1c091ca70b458938535b09
dalilasampaio <dalilasampaio6@gmail.com>
2022-06-24 01:54:48 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -28,13 +28,28 @@ public class Board {
         this.columns = columns;
     }
 
+    //Métodos da classe Board
+
+    //acessando a peça
     public Piece piece(int row, int column){
         return pieces[row][column];
     }
 
+    //acessando a posição da peça, ex: b5
     public Piece piece(Position position){
         return pieces[position.getRow()][position.getColumn()];
     }
 
+
+    //atribuindo uma determinada peça a uma determinada posição.
+    /* Nesse método temos um forte uso de Herança pois o metodo recebe
+    * um objeto Piece, mas o que será atribuido à ele, será
+    * uma classe(king, Rook) que extends ChessPiece que extends Piece.
+    * Logo, essa classe É uma ChessPiece e uma Piece e está sendo atribuída
+    * no método com o uso do UpCasting na Classe ChessMatch. */
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
 }
 
